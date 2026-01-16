7a778f2f4c62fac20879398bc742c26818c9d160
qinhuajun <qinhuajun@outlook.com>
2026-01-16 19:01:03 +0800

    fix login page icon bug

--- src/app/language-switcher/language-switcher.component.ts
+++ src/app/language-switcher/language-switcher.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NzDropdownModule } from 'ng-zorro-antd/dropdown';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzIconModule, NzIconService } from 'ng-zorro-antd/icon';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { GlobalOutline } from '@ant-design/icons-angular/icons';
 
 @Component({
   selector: 'app-language-switcher',
@@ -57,11 +59,15 @@ import { TranslateService, TranslateModule } from '@ngx-translate/core';
 })
 export class LanguageSwitcherComponent {
   private translate = inject(TranslateService);
+  private readonly iconService = inject(NzIconService);
 
   currentLanguage = 'zh';
 
   constructor() {
     this.currentLanguage = this.translate.getCurrentLang() || 'zh';
+    // Register the global icon
+    const icons: IconDefinition[] = [GlobalOutline];
+    this.iconService.addIcon(...icons);
   }
 
   changeLanguage(lang: string): void {
